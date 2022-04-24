d5542d5b030f44016143ce3f732c0c0f6abf34f6
Yannarp <yannarp@hotmail.com>
2022-04-24 13:31:07 -0300

    initial commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	
+	MONGO_CONNECTION:
+	'mongodb+srv://Yannarp:<password>@url-shotener-dio.jewhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
